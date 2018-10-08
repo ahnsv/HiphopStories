e5ab865120b8a43417f2472e564d85a53edc044d
Humphrey Ahn <ahnsv@bc.edu>
2018-10-08 14:58:56 +0900

    react native init

---
+++ psypher/App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation'
+import MainScreen from './Components/MainScreen'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <AppStackNavigator />
+    );
+  }
+
+}
+
+const AppStackNavigator = StackNavigator({
+
+  Main: {
+    screen: MainScreen
+  }
+})
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
